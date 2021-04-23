4ba4f51e57834cd2d587845ef231495c95d64ca9
Angga Bawole <bawoleangga03@gmail.com>
2021-04-23 22:02:51 +0800

    before testing new formula

--- app/Http/Controllers/ManganController.php
+++ app/Http/Controllers/ManganController.php
@@ -46,11 +46,43 @@ class ManganController extends Controller
         $jumlah = $restoran->rating->count();
         
         if ($restoran->rating->count() != 0)
-            $avg = $total / $jumlah;  
+            $avg = $total / $jumlah;
         else
             $avg = $request->rating;
 
-        $restoran->score = $avg;
+        // Σ n.f / Σ n
+        $score = $total / 15;
+
+        $restoran->score = $score;
+
+        // Test
+        $f1 = $restoran->rating->where('rating', '1');
+        $f2 = $restoran->rating->where('rating', '2');
+        $f3 = $restoran->rating->where('rating', '3');
+        $f4 = $restoran->rating->where('rating', '4');
+        $f5 = $restoran->rating->where('rating', '5');
+        
+        if ($f1) {
+            $f1 = $f1->sum('rating');
+        }
+        if ($f2) {
+            $f2 = $f2->sum('rating');
+        }
+        if ($f3) {
+            $f3 = $f3->sum('rating');
+        }
+        if ($f4) {
+            $f4 = $f4->sum('rating');
+        }
+        if ($f5) {
+            $f5 = $f5->sum('rating');
+        }
+
+        $Σfn = $f1 + $f2 + $f3 + $f4 + $f5;
+        $Σn = 15;
+        
+        $scor = $Σfn / $Σn;
+        // End of Test
         $restoran->save();
         
         return redirect()->back();
